90f04454efe212bde40323bcc016bb32c6d474c8
Marcela Catrian <Marcela Catrian@MARCELA1988>
2018-07-21 12:05:30 -0400

    Agregar archivos de proyecto.

---
+++ ArriendosAutos/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace ArriendosAutos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtAño = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Patente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Año = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.cmbTipo);
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnActualizar);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnGuardar);
+            this.groupBox1.Controls.Add(this.txtAño);
+            this.groupBox1.Controls.Add(this.txtModelo);
+            this.groupBox1.Controls.Add(this.txtMarca);
+            this.groupBox1.Controls.Add(this.txtPatente);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(25, 32);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(645, 187);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mantenedor";
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "",
+            "Gasolina",
+            "Petroleo",
+            "Electrico"});
+            this.cmbTipo.Location = new System.Drawing.Point(385, 102);
+            this.cmbTipo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(148, 23);
+            this.cmbTipo.TabIndex = 14;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Image = global::ArriendosAutos.Properties.Resources._573848;
+            this.btnLimpiar.Location = new System.Drawing.Point(447, 139);
+            this.btnLimpiar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(61, 41);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Image = global::ArriendosAutos.Properties.Resources.icons8_synchronize_48;
+            this.btnActualizar.Location = new System.Drawing.Point(516, 140);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(51, 41);
+            this.btnActualizar.TabIndex = 12;
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Image = global::ArriendosAutos.Properties.Resources._709519;
+            this.btnEliminar.Location = new System.Drawing.Point(575, 139);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(54, 41);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Image = global::ArriendosAutos.Properties.Resources.icons8_save_as_48;
+            this.btnGuardar.Location = new System.Drawing.Point(376, 140);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(63, 41);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtAño
+            // 
+            this.txtAño.Location = new System.Drawing.Point(551, 103);
+            this.txtAño.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtAño.Name = "txtAño";
+            this.txtAño.Size = new System.Drawing.Size(78, 23);
+            this.txtAño.TabIndex = 9;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(215, 103);
+            this.txtModelo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(156, 23);
+            this.txtModelo.TabIndex = 7;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(28, 102);
+            this.txtMarca.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(179, 23);
+            this.txtMarca.TabIndex = 6;
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.Location = new System.Drawing.Point(28, 53);
+            this.txtPatente.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(179, 23);
+            this.txtPatente.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(548, 86);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 14);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Año";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(386, 86);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tipo Combustible";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(212, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Modelo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marca";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patente";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.dgvListado);
+            this.groupBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(25, 250);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox2.Size = new System.Drawing.Size(645, 199);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listado";
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Patente,
+            this.Marca,
+            this.Modelo,
+            this.Tipo,
+            this.Año});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvListado.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvListado.Location = new System.Drawing.Point(28, 41);
+            this.dgvListado.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.Size = new System.Drawing.Size(601, 141);
+            this.dgvListado.TabIndex = 0;
+            this.dgvListado.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GrillaHacerClick);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "IdCodigo";
+            this.Id.Name = "Id";
+            // 
+            // Patente
+            // 
+            this.Patente.HeaderText = "Patente";
+            this.Patente.Name = "Patente";
+            // 
+            // Marca
+            // 
+            this.Marca.HeaderText = "Marca";
+            this.Marca.Name = "Marca";
+            // 
+            // Modelo
+            // 
+            this.Modelo.HeaderText = "Modelo";
+            this.Modelo.Name = "Modelo";
+            // 
+            // Tipo
+            // 
+            this.Tipo.HeaderText = "Tipo Combustible";
+            this.Tipo.Name = "Tipo";
+            // 
+            // Año
+            // 
+            this.Año.HeaderText = "Año";
+            this.Año.Name = "Año";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(723, 461);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Renta Car";
+            this.Load += new System.EventHandler(this.CargarLoad);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtAño;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvListado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Patente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Marca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modelo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Año;
+        private System.Windows.Forms.ComboBox cmbTipo;
+    }
+}
+
---
+++ ArriendosAutos/Form1.cs
@@ -0,0 +1,189 @@
+﻿using DatosAcceso;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArriendosAutos
+{
+    public partial class Form1 : Form
+    {
+        Autos _auto;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarLoad(object sender, EventArgs e)
+        {
+            CargarGrilla();
+            DesbloquearBoton();
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (var contexto = new VehiculosEntities())
+            {
+                contexto.Autos.Add(new Autos()
+                {
+                    Patente = txtPatente.Text,
+                    Marca = txtMarca.Text,
+                    Modelo = txtModelo.Text,
+                    TipoCombustible = cmbTipo.Text,
+                    Año = Convert.ToInt32(txtAño.Text),
+
+
+                });
+
+                contexto.SaveChanges();
+            }
+            CargarGrilla();
+            LimpiarFormulario();
+
+        }
+
+
+
+        private void CargarGrilla()
+        {
+
+            dgvListado.Rows.Clear();
+
+
+
+            using (VehiculosEntities contexto = new VehiculosEntities())
+            {
+
+                List<Autos> autos = contexto.Autos.ToList();
+
+
+                foreach (Autos auto in autos)
+                {
+
+                    dgvListado.Rows.Add(new object[] {
+                                auto.Id,
+                                auto.Patente,
+                                auto.Marca,
+                                auto.Modelo,
+                                auto.TipoCombustible,
+                                auto.Año
+                            }
+                        );
+
+
+                }
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+            DesbloquearBoton();
+        }
+
+        private void CargarInformacionAutosEnFormulario()
+        {
+            txtPatente.Text = _auto.Patente;
+            txtMarca.Text = _auto.Marca;
+            txtModelo.Text = _auto.Modelo;
+            cmbTipo.Text = _auto.TipoCombustible;
+            txtAño.Text = _auto.Año.ToString();
+        }
+        private void LimpiarFormulario()
+        {
+            txtPatente.Clear();
+            txtMarca.Clear();
+            txtModelo.Clear();
+            cmbTipo.SelectedIndex = 0;
+            txtAño.Clear();
+        }
+
+        private void GrillaHacerClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int row = e.RowIndex;
+            string id = dgvListado.Rows[row].Cells[0].Value.ToString();
+
+
+            int idAutos = Convert.ToInt32(id);
+            using (VehiculosEntities contexto = new VehiculosEntities())
+            {
+                _auto = contexto.Autos.Find(idAutos);
+
+                CargarInformacionAutosEnFormulario();
+
+                BloquearBoton();
+            }
+
+        }
+
+        private void BloquearBoton()
+        {
+            EstadoBoton(false);
+        }
+        private void DesbloquearBoton()
+        {
+            EstadoBoton(true);
+        }
+        private void EstadoBoton(bool estado)
+        {
+            btnActualizar.Enabled = !estado;
+            btnEliminar.Enabled = !estado;
+            btnGuardar.Enabled = estado;
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+
+            _auto.Patente = txtPatente.Text;
+            _auto.Marca = txtMarca.Text;
+            _auto.Modelo = txtModelo.Text;
+            _auto.TipoCombustible = cmbTipo.Text;
+            _auto.Año = Convert.ToInt32(txtAño.Text);
+            
+
+            using (VehiculosEntities contexto = new VehiculosEntities())
+            {
+                contexto.Entry(_auto).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+
+            }
+            CargarGrilla();
+            LimpiarFormulario();
+            DesbloquearBoton();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            
+
+                DialogResult respuesta = MessageBox.Show("Esta seguro de eliminar auto " + _auto.Patente, "Sistema Renta Car", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (respuesta == DialogResult.Yes)
+                {
+                    using (VehiculosEntities contexto = new VehiculosEntities())
+                    {
+                        contexto.Entry(_auto).State = System.Data.Entity.EntityState.Deleted;
+                        contexto.SaveChanges();
+                    }
+                    CargarGrilla();
+                    LimpiarFormulario();
+                    DesbloquearBoton();
+                }
+                else
+                {
+                    txtPatente.Select();
+                    txtPatente.Focus();
+                }
+
+
+            
+        }
+    }
+}
